bac43b4a494dbeb4cdad1b6f90dad9d4645c393f
vivekviva-1992 <vivekms.tpr@gmail.com>
2024-10-29 11:41:45 +0530

    Add files via upload

---
+++ utils/appColors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class TodoColor {
+  static const primary = Color(0xFFF6EEE2);
+  static const headerText = Color(0xFF6F1D42);
+  static const buttonColor = Color(0xFF6C1D45);
+  static const descColor = Color(0xFF5B555C);
+  static const statusColor = Color(0xFFD6A461);
+}
